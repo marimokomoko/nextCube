d6cfbb54843e7e3e0e8164cce503bc5e44a8435d
marimokomoko <mari_n17@yahoo.co.jp>
2024-08-31 21:34:38 +0900

    blogデータ取得関数作成

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -4,3 +4,16 @@ export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN as string,
   apiKey: process.env.API_KEY as string,
 })
+
+export async function getPostBySlug(slug: string): Promise<any | undefined> {
+  try {
+    const post = await client.get({
+      endpoint: "blogs",
+      queries: { filters: `slug[equals]${slug}` },
+    })
+    return post.contents[0]
+  } catch (err) {
+    console.log("----- getPostBySlug -----")
+    console.log(err)
+  }
+}
