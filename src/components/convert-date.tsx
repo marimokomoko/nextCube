03ea0a3099a258be9769c453f194fb9400f2c2b2
marimokomoko <mari_n17@yahoo.co.jp>
2024-09-14 21:45:58 +0900

    dateISO が無効な場合にはデフォルトの表示やエラーメッセージを表示するように修正

--- src/components/convert-date.tsx
+++ src/components/convert-date.tsx
@@ -7,6 +7,11 @@ type ConvertDateProps = {
 }
 
 export default function ConvertDate({ dateISO }: ConvertDateProps) {
+  // dateISOが有効かチェックし、無効な場合は "無効な日付" を表示
+  if (!dateISO || isNaN(Date.parse(dateISO))) {
+    return <time>無効な日付</time>
+  }
+
   return (
     <time dateTime={dateISO}>
       {format(parseISO(dateISO), "yyyy年MM月dd日", {
